344ce1303c57ad1c95cfc57eff08d09576e5ebf9
akshay chavan <akshaychavan44.ac@gmail.com>
2026-07-31 22:16:46 +0530

    Remove unnecessary comments

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,18 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// Connect MongoDB
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/tickets", ticketRoutes);
 
 
-// Test Route
 app.get("/", (req, res) => {
     res.send("DataStraw Backend Running");
 });
 
 
-// Server Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
